53aa68d25aa7c8a5770ae8124e57a9b6933c2db2
Larissa kneefel gmd1a <jade_ketan@hotmail.com>
2016-10-08 18:23:05 +0200

    testscene folder

---
+++ deltion project 1/Assets/testscene/CameraView.js
@@ -0,0 +1,11 @@
+﻿#pragma strict
+	var lookUp	:Vector3;
+function Start () {
+
+}
+
+function Update () {
+	lookUp.x=(Input.GetAxis("Mouse Y"))*-100* Time.deltaTime;
+	
+	transform.Rotate(lookUp);	
+}
---
+++ deltion project 1/Assets/testscene/Player.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+	var move: Vector3;
+	var look :Vector3;
+function Start () {
+
+}
+
+function Update () {
+	
+	
+	move.x= Input.GetAxis("Horizontal")*10*Time.deltaTime;
+	move.z=Input.GetAxis("Vertical")*10*Time.deltaTime;
+	transform.Translate(move);
+	//view
+	look.y=(Input.GetAxis("Mouse X"))*100* Time.deltaTime;
+	transform.Rotate(look);
+}
+
